0ff69bc9dfd6a0128b47eed75d84c57376161739
Christos Drogidis <4207994+ascoos@users.noreply.github.com>
2026-05-10 22:08:56 +0300

    Delete conf directory

--- conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'jsql' => [
-        'config_path' => '/root/path/conf/config.json', 
-        'users_path' => '/root/path/conf/users.json',
-        'databases_root_path' => '/root/path/jsql_db',      
-    ]
-];
-?>
